a1b24eb9166edf6a5b07ea20d582465b6eacc372
Xavier Cruz <68714224+XavierCruz5106@users.noreply.github.com>
2024-12-06 11:40:42 -0500

    feat: adds day_3 part1 and 2

---
+++ day_3/part01/Main.java
@@ -0,0 +1,7 @@
+package day_3.part01;
+
+public class Main {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ day_3/part02/Main.java
@@ -0,0 +1,7 @@
+package day_3.part02;
+
+public class Main {
+  public static void main(String[] args) {
+
+  }
+}
